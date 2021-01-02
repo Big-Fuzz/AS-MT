de4856550af511810d3fbacfe5acdc12017b0db6
Big-Fuzz <60523786+Big-Fuzz@users.noreply.github.com>
2021-01-02 23:31:07 +0100

    Add files via upload

---
+++ NN+NN_extended_0_9/emp_compare.py
@@ -0,0 +1,38 @@
+from utils_tf2 import * 
+import numpy as np
+import tensorflow as tf
+import math
+import pickle
+
+'''
+This program calculates the average error across a dataset using the empirical formula:
+'''
+
+tf.compat.v1.disable_eager_execution()
+tf.compat.v1.reset_default_graph()                # to be able to rerun the model without overwriting tf variables
+
+data_ = "datasets/Data_NN_+_0_9.csv"
+list_of_inputs = ['permittivity', 'patch_length','patch_width','substrate_height','feed_depth','feed_gap_width']
+list_of_outputs = ['resonance']
+
+data = pd.read_csv(data_)
+data_x, data_y = separate_dataset(data, list_of_inputs, list_of_outputs)
+
+(n_x, m) = data_x.shape                          # (n_x: input size, m : number of examples in the train set)
+n_y = data_y.shape[0]                            # n_y : output size
+    
+err_sum = 0
+for i in range(0, m):
+    sample_num = i
+    tru_val = data_y[:, sample_num]
+    sample_L = data_x[:, sample_num][1]
+    sample_W = data_x[:, sample_num][2]
+    sample_h = data_x[:, sample_num][3]
+    sample_e_r = data_x[:, sample_num][0]
+    if data_ == "datasets/Data_with_header.csv":
+        eq = resonance_emp(sample_L, sample_W, sample_h, sample_e_r, mm = True)
+    else:
+        eq = resonance_emp(sample_L, sample_W, sample_h, sample_e_r, mm = True) / 10**9
+    err = (abs(tru_val - eq)) / (abs(tru_val))
+    err_sum = err_sum + err
+print ("The average accuracy of the equation: ", 1 - err_sum[0] / m )    
---
+++ NN+NN_extended_0_9/resonance_nn.py
@@ -0,0 +1,49 @@
+from utils_tf2 import* 
+import numpy as np
+import tensorflow as tf
+import pickle
+
+'''
+This is a Neural Network to predict the resonant frequency, The structure is:
+Train/Test split = 0.85
+hidden layers = 5
+neurons in each layer = 6
+learning rate = 0.0085
+number of epochs = 3600000
+'''
+
+# Specify Inputs, Outputs, Train/Test split, and Datafile (arguments of the load_dataset function)
+data = "datasets/Data_NN_+_0_9.csv"
+list_of_inputs = [ 'permittivity', 'patch_length','patch_width','substrate_height','feed_depth','feed_gap_width']
+list_of_outputs = ['resonance']
+W = [9, 9, 9, 9, 9, 1]
+activations = ["relu", "relu", "relu", "relu", "relu"]
+frac = 0.85
+norm_mean = "normalizations/resonance_NN_+_0_9_mean_value.pkl"
+norm_std = "normalizations/resonance_NN_+_0_9_std_value.pkl"
+param = "parameters/resonance_NN_+_0_9_parameters.pkl"
+
+# Obtain Train and Test sets inputs and outputs and normalize inputs
+train_set_x, train_set_y, test_set_x, test_set_y = load_dataset(data, frac, list_of_inputs, list_of_outputs, consistent = True, seed = 100)
+train_set_x, test_set_x = normalize_inputs(train_set_x, test_set_x, name_mean = norm_mean, name_std = norm_std)
+
+# Structure and build the NN by defining the number of layers, neurons, and activations
+parameters = model(train_set_x, train_set_y, test_set_x, test_set_y, W, activations,learning_rate = 0.0085, num_epochs = 360000)
+
+# Specify sample to test and predict
+sample_num = 5
+sample = np.reshape(test_set_x[:, sample_num], [test_set_x.shape[0], 1])
+print("the sample is: ", sample)
+print(sample.shape)
+
+# Predict 
+my_prediction = predict(sample, parameters, activations)
+print("The prediction is", my_prediction)
+print("The true value is: ",test_set_y[:, sample_num] )
+
+# Save parameters as binary into .pkl file
+f = open(param, 'wb')
+pickle.dump(parameters, f)
+f.close()
+print("Parameters have been saved")
+
